de46079f11ced9b65962fd31409700a04ec419df
usagisah <usagisah@163.com>
2024-03-11 01:10:09 +0800

    优化日志打印逻辑

--- packages/album/src/logger/format.ts
+++ packages/album/src/logger/format.ts
@@ -1,5 +1,4 @@
 import { gray, green, magenta, red, yellow } from "@albumjs/tools/lib/colorette"
-import day from "@albumjs/tools/lib/dayjs"
 import { format as prettyFormat } from "@albumjs/tools/lib/pretty-format"
 
 const colors = { log: green, info: green, warn: yellow, error: red, debug: magenta }
@@ -9,7 +8,6 @@ export function getColor(type: keyof typeof colors | (string & {})) {
 
 export function formatConsoleMessage(level: string, context: string | null, messages: string[]) {
   const c = getColor(level)
-  const t = day().format("YYYY-MM-DD HH:mm:ss")
   const m = messages.map(m => (typeof m === "string" ? m : prettyFormat(m, { highlight: true }))).join("")
-  return `${c("[" + level + "]")} ${red(t)} ${context ? gray("[" + context + "]") : ""} -> ${c(m)}\n`
+  return `${red("[" + level + "]")} ${context ? gray("[" + context + "]") : ""} -> ${c(m)}\n`
 }
