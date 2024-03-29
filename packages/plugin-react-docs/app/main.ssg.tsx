04108b4230f52b011f4da11c61564f57501164f7
usagisah <usagisah@163.com>
2024-03-29 22:20:49 +0800

    添加 emotion server 样式提取能力

--- packages/plugin-react-docs/app/main.ssg.tsx
+++ packages/plugin-react-docs/app/main.ssg.tsx
@@ -1,8 +1,7 @@
 import { SiteConfig } from "@docs/site-config"
 import { readFile } from "fs/promises"
 import { dirname, join } from "path"
-import { renderToStaticNodeStream } from "react-dom/server"
-import { Writable } from "stream"
+import { renderToString } from "react-dom/server"
 import { fileURLToPath } from "url"
 import { createApp } from "./createApp"
 
@@ -14,49 +13,51 @@ interface SSGRenderOption {
   entryPath: string
 }
 
+import createCache from "@emotion/cache"
+import { CacheProvider } from "@emotion/react"
+import createEmotionServer from "@emotion/server/create-instance"
+const key = "custom"
+const cache = createCache({ key })
+const { extractCriticalToChunks, constructStyleTagsFromChunks } = createEmotionServer(cache)
+
 const __dirname = dirname(fileURLToPath(import.meta.url))
-export function ssgRender({ siteConfig, entryPath, importPath, contentPath, scripts }: SSGRenderOption) {
-  return new Promise(async send => {
-    const [css1, css2, { default: MDContent }] = await Promise.all([
-      readFile(join(__dirname, "./normalize.css")),
-      readFile(join(__dirname, "./docs.css")),
-      import(/*@vite-ignore*/ importPath)
-    ])
-    siteConfig.frontmatter = MDContent.frontmatter
-    const App = await createApp(siteConfig, MDContent)
-    const HTML = () => {
-      return (
-        <html lang="en">
-          <head>
-            <meta charSet="UTF-8" />
-            <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>{siteConfig.title}</title>
-            <style data-type="normalize.css" dangerouslySetInnerHTML={{ __html: css1 }}></style>
-            <style data-type="docs.css" dangerouslySetInnerHTML={{ __html: css2 }}></style>
-          </head>
-          <body>
-            <div id="album-docs">
-              <App />
-            </div>
-            <script type="text/json" id="_docs-meta" dangerouslySetInnerHTML={{ __html: JSON.stringify({ contentPath }) }}></script>
-            <script type="module" src={entryPath}></script>
-          </body>
-        </html>
-      )
-    }
-    let html = ""
-    renderToStaticNodeStream(<HTML />).pipe(
-      new Writable({
-        write(chunk, encoding, callback) {
-          html += chunk.toString()
-          callback()
-        },
-        final(cb) {
-          send(html)
-          cb()
-        }
-      })
+export async function ssgRender({ siteConfig, entryPath, importPath, contentPath, scripts }: SSGRenderOption) {
+  const [css1, css2, { default: MDContent }] = await Promise.all([
+    readFile(join(__dirname, "./normalize.css")),
+    readFile(join(__dirname, "./docs.css")),
+    import(/*@vite-ignore*/ importPath)
+  ])
+  siteConfig.frontmatter = MDContent.frontmatter
+  const App = await createApp(siteConfig, MDContent)
+  const stylePlaceholder = "__$_" + Date.now()
+  const HTML = () => {
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="UTF-8" />
+          <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>{siteConfig.title}</title>
+          <style data-type="normalize.css" dangerouslySetInnerHTML={{ __html: css1 }}></style>
+          <style data-type="docs.css" dangerouslySetInnerHTML={{ __html: css2 }}></style>
+          {stylePlaceholder}
+        </head>
+        <body>
+          <div id="album-docs">
+            <App />
+          </div>
+          <script type="text/json" id="_docs-meta" dangerouslySetInnerHTML={{ __html: JSON.stringify({ contentPath }) }}></script>
+          <script type="module" src={entryPath}></script>
+        </body>
+      </html>
     )
-  })
+  }
+  const html = renderToString(
+    <CacheProvider value={cache}>
+      <HTML />
+    </CacheProvider>
+  )
+  const chunks = extractCriticalToChunks(html)
+  const styles = constructStyleTagsFromChunks(chunks)
+  return html.replace(stylePlaceholder, styles)
 }
